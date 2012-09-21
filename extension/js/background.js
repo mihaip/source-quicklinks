a93fc64075c76040fa4d7f506ff83e795956fcf8
Mihai Parparita <mihaip@chromium.org>
2012-09-21 16:59:20 -0400

    Add an "sql" omnibox keyword for triggering code searches.

--- extension/js/background.js
+++ extension/js/background.js
@@ -31,3 +31,39 @@ chrome.extension.onMessage.addListener(function(message, sender) {
     }
   }
 });
+
+function getCodeSearchUrl(query) {
+  return 'http://code.google.com/p/chromium/source/search?q=' + query;
+}
+
+chrome.omnibox.setDefaultSuggestion({
+  description: '<match>%s</match> in the Chromium repository'
+});
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+  suggest([
+    {
+      content: getCodeSearchUrl(text + ' -file:^src/third_party -file:^src/v8'),
+      description: '<match>' + text + '</match> in the Chromium repository (no dependencies)'
+    },
+    {
+      content: getCodeSearchUrl('file:^src/third_party/WebKit ' + text),
+      description: '<match>' + text + '</match> in the WebKit repository'
+    },
+    {
+      content: getCodeSearchUrl('file:' + text),
+      description: '<match>' + text + '</match> in file names'
+    },
+  ]);
+});
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  var url;
+
+  if (text.indexOf('http') == 0) {
+    url = text;
+  } else {
+    url = getCodeSearchUrl(text);
+  }
+  chrome.tabs.update({url: url});
+});
